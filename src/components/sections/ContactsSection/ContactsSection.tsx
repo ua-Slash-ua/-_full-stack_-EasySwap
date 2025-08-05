312bd54138b0b5acd1a4a592bcfe1db833e5fbcd
ua-Slash-ua <so.slashua@gmail.com>
2025-08-05 15:15:20 +0300

    f-e Add ContactsSection Part-3

--- src/components/sections/ContactsSection/ContactsSection.tsx
+++ src/components/sections/ContactsSection/ContactsSection.tsx
@@ -1,23 +1,30 @@
 import s from './ContactsSection.module.css'
-import LocationItem from '@/components/sections/ApplicationSection/LocationItem/LocationItem'
+import LocationItem from '@/components/sections/ContactsSection/LocationItem/LocationItem'
+import SocialNetworkItem from '@/components/sections/ContactsSection/SocialNetworkItem/SocialNetworkItem'
 
 export default function ContactsSection({ block, locale }: { block: any; locale: string }) {
-  const data: any[] = block.locations
+  const locations: any[] = block.locations
+  const social_networks: any = block.social_networks
   return (
     <>
       <section className={s.contacts_section} id="contacts">
         <aside className={s.main}>
           <div className={s.content}>
             <h3>Контакти і локації</h3>
-            <ul className={s.content_list}>
-              {data.map((item, index) =>
+            <ul className={s.content_list_address}>
+              {locations.map((item, index) =>
                 item.is_location ? <LocationItem key={index} {...item} /> : '',
               )}
             </ul>
+
           </div>
-          <div className={s.social_network}></div>
+          <ul className={s.content_list_social}>
+            {Object.entries(social_networks).map(([key, value], index) => (
+              <SocialNetworkItem key={index} {...social_networks[key]} />
+            ))}
+          </ul>
         </aside>
-        <div className={s.map}></div>
+        <div className={s.map}> MAP</div>
       </section>
     </>
   )
