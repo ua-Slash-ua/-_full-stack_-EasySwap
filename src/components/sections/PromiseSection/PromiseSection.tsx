82aee8004e7042859ade32a1b2f87d21092874e7
ua-Slash-ua <so.slashua@gmail.com>
2025-08-18 17:42:07 +0300

    Add animations on promiseSection

--- src/components/sections/PromiseSection/PromiseSection.tsx
+++ src/components/sections/PromiseSection/PromiseSection.tsx
@@ -3,24 +3,30 @@ import s from './PromiseSection.module.css'
 
 export default function PromiseSection({ block, locale }: { block: any; locale: string }) {
   const items = [
-    { text: block.title, svg: block.icon, word: block.word},
-    { text: block.title, svg: block.icon, word: block.word},
-    { text: block.title, svg: block.icon, word: block.word},
+    { text: block.title, svg: block.icon, word: block.word },
+    { text: block.title, svg: block.icon, word: block.word },
+    { text: block.title, svg: block.icon, word: block.word },
   ]
 
   return (
     <>
       <section className={s.promise_section}>
         <div className={s.promise_container}>
-          <div className={s.promise_item}>
-            <p>{block.title}</p>
-            <div className={s.promise}>
-              <div
-                dangerouslySetInnerHTML={{ __html: block.icon }}
-              />
-              <span>{block.word}</span>
-            </div>
+          <div className={s.scroller}>
+            {[...items, ...items].map((item, index) => (
+              <div key={index} className={s.promise_item}>
+                <p>{item.text}</p>
+                <div className={s.promise}>
+                  <div dangerouslySetInnerHTML={{ __html: item.svg }} />
+                  <span>{item.word}</span>
+                </div>
+              </div>
+            ))}
           </div>
+        </div>
+        <div className={s.blur}>
+          <div className={s.blur_item}></div>
+          <div className={s.blur_item}></div>
         </div>
       </section>
     </>
