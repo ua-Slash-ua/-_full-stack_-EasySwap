d0c0807c08e29c6d4fda1d976a7ec5bc60a82855
ua-Slash-ua <so.slashua@gmail.com>
2025-08-22 13:04:45 +0300

    Mobile:Add Header Scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,50 +10,64 @@ import HeaderScroll from '@/components/Header/HeaderScroll/HeaderScroll'
 import { useEffect, useState } from 'react'
 
 export default function Header({ block, locale }: { block: any; locale: string }) {
-  const [scrolled, setScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(false)
+  const [width, setWidth] = useState<number>(0)
 
   useEffect(() => {
     const handleScroll = () => {
       // розрахунок 10vw у пікселях
-      const threshold = window.innerWidth * 0.4693;
+      const threshold = window.innerWidth * 0.4693
       // const threshold = window.innerWidth * 0.1;
-      const currentScrollY = window.scrollY;
+      const currentScrollY = window.scrollY
 
       if (currentScrollY > threshold) {
-        setScrolled(true);
+        setScrolled(true)
       } else {
-        setScrolled(false);
+        setScrolled(false)
       }
-    };
+    }
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
     // одразу викликаємо для першого рендера
-    handleScroll();
+    handleScroll()
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth / 4)
+    handleResize() // виставляємо ширину одразу після маунту
 
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   return (
     <>
-      <header
-        className={`${s.header} ${!scrolled ? s.visible : s.hidden}`}
-      >
+      <header className={`${s.header} ${!scrolled ? s.visible : s.hidden}`}>
         <div className={s.rside}>
           <Logo />
-          <Menu />
+          {width > 1024 ? <Menu /> : null}
         </div>
         <div className={s.lside}>
-          <BtnPhone
-            svgIcon={contacts.iconPhone.replace('#7C4DF5', 'white')}
-            phone={block.phone}
-          />
-          <BtnSendApplication
-            svgIcon={contacts.iconMail}
-            text={'Залишити заявку'}
-          />
+          {width > 1024 ? (
+            <BtnPhone
+              svgIcon={contacts.iconPhone.replace('#7C4DF5', 'white')}
+              phone={block.phone}
+            />
+          ) : (
+            <BtnPhone
+              svgIcon={contacts.iconPhone.replace('#7C4DF5', 'white')}
+              phone={block.phone}
+              isReversed={true}
+            />
+          )}
+          {width > 1024 ? (
+            <BtnSendApplication svgIcon={contacts.iconMail} text={'Залишити заявку'} />
+          ) : (
+            <div className={s.header_menu}><span>Меню</span></div>
+          )}
         </div>
       </header>
 
@@ -63,6 +77,5 @@ export default function Header({ block, locale }: { block: any; locale: string }
         className={`${s.headerScroll} ${scrolled ? s.visible : s.hidden}`}
       />
     </>
-
   )
 }
