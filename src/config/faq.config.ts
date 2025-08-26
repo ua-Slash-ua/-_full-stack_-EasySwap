6a9f9107792c151e24ea109706b534426e3fe9bf
ua-Slash-ua <so.slashua@gmail.com>
2025-08-26 11:55:39 +0300

    Bug-fix Part-2

--- src/config/faq.config.ts
+++ src/config/faq.config.ts
@@ -1,16 +1,8 @@
 export const faqIcons = {
-  active:'<svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-    '<rect width="44" height="44" rx="22" fill="currentColor"/>\n' +
-    '<path d="M29 22H22M22 22H15M22 22V15M22 22V29" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-    '<defs>\n' +
-    '<linearGradient id="paint0_linear_609_1801" x1="-12.375" y1="-6.875" x2="22" y2="44" gradientUnits="userSpaceOnUse">\n' +
-    '<stop stop-color="#B792FF"/>\n' +
-    '<stop offset="0.990385" stop-color="currentColor"/>\n' +
-    '</linearGradient>\n' +
-    '</defs>\n' +
-    '</svg>\n',
-  disabled:'<svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-    '<rect width="44" height="44" rx="22" fill="currentColor"/>\n' +
-    '<path d="M29 22H22H15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-    '</svg>\n',
+  active:'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="2" viewBox="0 0 16 2" fill="none">\n' +
+    '  <path d="M15 1H8H1" stroke="#622FF1" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+    '</svg>',
+  disabled:'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">\n' +
+    '  <path d="M15 8H8M8 8H1M8 8V1M8 8V15" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+    '</svg>',
 };
